507fbce6cd656d2a399df8269400eb534519c71a
vvolkov1411 <vvolkov1411@gmail.com>
2023-08-12 20:35:14 +0600

    as

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,15 @@ const App = () => {
   return (
     <div>
       <button onClick={onClose}>Close</button>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda,
+        consequatur.
+      </p>
       <p>{tg.user?.username}</p>
       <p>{tg.user?.first_name}</p>
       <p>{tg.user?.last_name}</p>
       <p>{tg.user?.is_premium}</p>
+      <p>{tg.hash}</p>
     </div>
   );
 };
