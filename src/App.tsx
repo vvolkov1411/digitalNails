fbab624dfd35489fa5d6814c27ae53f155f999f3
vvolkov1411 <vvolkov1411@gmail.com>
2023-08-12 21:03:28 +0600

    lovely

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   useEffect(() => {
     tg.ready();
     tg.expand();
-    alert(tg.initDataUnsafe.user.photo_url);
+    alert(tg.user?.photo_url);
   }, []);
 
   const onClose = () => {
@@ -15,11 +15,7 @@ const App = () => {
   return (
     <div>
       <p>{tg.initDataUnsafe.user.photo_url}</p>
-      <img
-        src={tg.initDataUnsafe.user.photo_url}
-        style={{ width: 40, height: 40 }}
-        alt=""
-      />
+      <img src={tg.user?.photo_url} style={{ width: 40, height: 40 }} alt="" />
       <p>{tg.initDataUnsafe.user.first_name}</p>
       <p>{tg.hash}</p>
     </div>
