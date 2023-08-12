596270eec289504f351b30e8b3d8c3a0e4235615
vvolkov1411 <vvolkov1411@gmail.com>
2023-08-12 19:49:22 +0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+const tg = window.Telegram.WebApp;
+
+const App = () => {
+  useEffect(() => {
+    tg.ready();
+  });
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+export default App;
