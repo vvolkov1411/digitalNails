b7adffb579cc0593a7259a3d0c09711f0d3f81f7
vvolkov1411 <vvolkov1411@gmail.com>
2023-08-14 02:58:28 +0600

    dsfsdf

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
         </svg>
       </div>
       <div className="userName">
-        <p>{tg.WebAppUser.username}</p>
+        <p>{tg.WebAppUser?.username}</p>
         {tg.initDataUnsafe.user?.first_name ?? "alfabank"}{" "}
       </div>
     </div>
